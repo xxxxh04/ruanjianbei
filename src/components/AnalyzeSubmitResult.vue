51f133c71fe997da23114e6904c3876e6537d479
pH59x <ymoonl@outlook.com>
2024-05-27 11:01:30 +0800

    perf: 数据展示样式调整

--- src/components/AnalyzeSubmitResult.vue
+++ src/components/AnalyzeSubmitResult.vue
@@ -1,70 +1,70 @@
 <template>
     <div class="analyzer-data-view">
-
+        <el-container>
             <el-header>提交结果</el-header>
             <el-main>
                 <el-row :gutter="20">
-                    <el-col :span="6" class="analyzer-data-label">
+                    <el-col :span="8" class="analyzer-data-label">
                         <div class="grid-content ep-bg-purple">
                             <span>运行超时</span>
                         </div>
                     </el-col>
-                    <el-col :span="6" class="analyzer-data-value">
+                    <el-col :span="16" class="analyzer-data-value">
                         <div class="grid-content ep-bg-purple">
                             <span>{{ count.timeOut }}</span>
                         </div>
                     </el-col>
                 </el-row>
                 <el-row :gutter="20">
-                    <el-col :span="6" class="analyzer-data-label">
+                    <el-col :span="8" class="analyzer-data-label">
                         <div class="grid-content ep-bg-purple">
                             <span>答案正确</span>
                         </div>
                     </el-col>
-                    <el-col :span="6" class="analyzer-data-value">
+                    <el-col :span="16" class="analyzer-data-value">
                         <div class="grid-content ep-bg-purple">
                             <span>{{ count.correct }}</span>
                         </div>
                     </el-col>
                 </el-row>
                 <el-row :gutter="20">
-                    <el-col :span="6" class="analyzer-data-label">
+                    <el-col :span="8" class="analyzer-data-label">
                         <div class="grid-content ep-bg-purple">
                             <span>内存超限</span>
                         </div>
                     </el-col>
-                    <el-col :span="6" class="analyzer-data-value">
+                    <el-col :span="16" class="analyzer-data-value">
                         <div class="grid-content ep-bg-purple">
                             <span>{{ count.outOfMem }}</span>
                         </div>
                     </el-col>
                 </el-row>
                 <el-row :gutter="20">
-                    <el-col :span="6" class="analyzer-data-label">
+                    <el-col :span="8" class="analyzer-data-label">
                         <div class="grid-content ep-bg-purple">
                             <span>编译错误</span>
                         </div>
                     </el-col>
-                    <el-col :span="6" class="analyzer-data-value">
+                    <el-col :span="16" class="analyzer-data-value">
                         <div class="grid-content ep-bg-purple">
                             <span>{{ count.complieErr }}</span>
                         </div>
                     </el-col>
                 </el-row>
                 <el-row :gutter="20">
-                    <el-col :span="6" class="analyzer-data-label">
+                    <el-col :span="8" class="analyzer-data-label">
                         <div class="grid-content ep-bg-purple">
                             <span>其他</span>
                         </div>
                     </el-col>
-                    <el-col :span="6" class="analyzer-data-value">
+                    <el-col :span="16" class="analyzer-data-value">
                         <div class="grid-content ep-bg-purple">
                             <span>{{  count.other }}</span>
                         </div>
                     </el-col>
                 </el-row>
             </el-main>
-
+        </el-container>
     </div>
 </template>
 
