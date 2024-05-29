8f5f6d73f0b88e066b77447515f0c1d9ff42df7c
ph59x <ymoonl@outlook.com>
2024-05-29 22:22:42 +0800

    feat: AI代码分析结果展示

---
+++ src/components/AnalyzeCodeProblem.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="analyzer-data-view">
+        <el-header>代码分析(由AI生成)</el-header>
+        <el-table :data="tableData" style="width: 100%">
+            <el-table-column prop="tag" label="tag" width="180" />
+            <el-table-column prop="count" label="count" width="180" />
+        </el-table>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+
+const tableData = [
+    {
+        tag: '函数参数默认值位置错误',
+        count: 114
+    },
+    {
+        tag: '忘记添加分号',
+        count: 514
+    },
+    {
+        tag: '只声明不实现',
+        count: 1919
+    },
+    {
+        tag: '内存泄漏',
+        count: 810
+    },
+    {
+        tag: '数组越界',
+        count: 467
+    },
+    {
+        tag: '类型转换问题',
+        count: 25
+    },
+    {
+        tag: '循环条件错误',
+        count: 911
+    },
+    {
+        tag: '函数调用顺序错误',
+        count: 1437
+    },
+    {
+        tag: '占位符1',
+        count: 138
+    }
+]
+
+</script>
+
+<style>
+
+</style>
--- src/views/AnalyzeView.vue
+++ src/views/AnalyzeView.vue
@@ -1,12 +1,13 @@
 <template>
     <div style="height: 200px; width: 200px;">
         <AnalyzeSubmitResult />
+        <AnalyzeCodeProblem />
     </div>
 </template>
 
 <script setup lang="ts">
 import AnalyzeSubmitResult from '@/components/AnalyzeSubmitResult.vue';
-
+import AnalyzeCodeProblem from '@/components/AnalyzeCodeProblem.vue'
 
 </script>
 
